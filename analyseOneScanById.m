36f15ae675c39673a8efd0432d8bff667787501d
cuillerj <jean.cuiller@wanadoo.fr>
2015-11-13 12:17:14 +0100

    The 360° version

--- analyseOneScanById.m
+++ analyseOneScanById.m
@@ -6,9 +6,10 @@ valAngle=180/(nbPasRotation-1);
 load all_theta
 load analyseMat.mat
 predLoc = predictOneVsAll(all_theta, analyseMat)
-c = floor(predLoc/(nbPasRotation+1));
+c = floor((predLoc-1)/(2*nbPasRotation-2))
 Maille=c+1
-Angle=((predLoc-c*(nbPasRotation)-1))*valAngle
+reste=predLoc-c*(2*nbPasRotation-2);
+Angle=mod(360-(reste-1)*valAngle,360)
 load 'extScanResult.mat'
 [x,y]=find(extScanResult(:,5)==predLoc);
 extScanResult(x:x+14,:);
