4aa9514951a487b55907a575b6f6a1f062461faf
cuillerj <jean.cuiller@wanadoo.fr>
2017-09-10 19:20:49 +0200

    narrow path development

---
+++ testMoveAcrossNarrowPath.m
@@ -0,0 +1,25 @@
+function [apRobot,robot] = testMoveAcrossNarrowPath(apRobot,robot,pathDistance,reqMove);
+ width=78;
+ length=80;
+ echoToGet=100;
+ northHeading=49;
+ startToEntryDistance=80;
+ robotStatus=robot.runningStatus;
+ debugOn=1;
+ if (robotStatus<=0)
+          printf(mfilename);
+          printf(" wait for robot to be ready  *** ");
+          printf(ctime(time()));
+  endif
+  while (robotStatus<=0)              % wait for robot to be ready
+          robotStatus=robot.runningStatus;
+          pause(2);
+          printf(".");
+  end
+ pause(1);
+robot.MoveAcrossNarrowPass(pathDistance,width,length,startToEntryDistance,northHeading,reqMove,echoToGet);
+apRobot = setfield(apRobot,"waitFor",robot.moveAcrossPassEnded);
+[apRobot,robot,retCodeMove]=ApWaitForRobot(apRobot,robot,debugOn);
+robot.RequestNarrowPathMesurments;
+pause(2);
+robot.RequestNarrowPathEchos;
